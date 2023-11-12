b5be1c66e254d138f5a5dfd980335eeb9bf49765
Krushiler <daniil.kn2004@gmail.com>
2023-11-13 00:04:44 +0800

    Change tests to reach 1000000% coveRAGE

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,6 +1,9 @@
 import pytest
 
 from interpreter import Interpreter
+from interpreter.ast import Node, Number, UnaryOp, BinOp
+from interpreter.parser import Parser
+from interpreter.token import TokenType, Token
 
 
 @pytest.fixture(scope="function")
@@ -8,51 +11,101 @@ def interpreter():
     return Interpreter()
 
 
+def run_interpreter(interpreter: Interpreter, code: str) -> str:
+    return interpreter.eval(f'BEGIN\n{code}\nEND.')
+
+
 class TestInterpreter:
-    interpreter = Interpreter()
 
     def test_add(self, interpreter):
-        assert interpreter.eval("2+2") == 4
+        assert run_interpreter(interpreter, "x:=2+2") == {'x': 4}
 
     def test_sub(self, interpreter):
-        assert interpreter.eval("2-2") == 0
+        assert run_interpreter(interpreter, "x:=2-2") == {'x': 0}
 
-    def test_add_with_letter(self, interpreter):
-        with pytest.raises(SyntaxError):
-            interpreter.eval("2+a")
+    def test_mul(self, interpreter):
+        assert run_interpreter(interpreter, "x:=2*2") == {'x': 4}
 
     def test_wrong_operator(self, interpreter):
         with pytest.raises(SyntaxError):
-            interpreter.eval("2&3")
+            run_interpreter(interpreter, "2&3")
 
-    @pytest.mark.parametrize(
-        "interpreter, code", [(interpreter, "2 + 2"),
-                              (interpreter, "2 +2 "),
-                              (interpreter, " 2+2")]
-    )
-    def test_add_spaces(self, interpreter, code):
-        assert interpreter.eval(code) == 4
-
-    def test_mul(self, interpreter):
-        assert interpreter.eval("2*2") == 4
+    def test_add_spaces(self, interpreter):
+        assert run_interpreter(interpreter, "x:=2 +      2") == {'x': 4}
 
     def test_div(self, interpreter):
-        assert interpreter.eval("2/2") == 1
+        assert run_interpreter(interpreter, "x:=2/2") == {'x': 1}
 
     def test_unary_minus(self, interpreter):
-        assert interpreter.eval("-2") == -2
+        assert run_interpreter(interpreter, "x:=-2") == {'x': -2}
 
     def test_unary_plus(self, interpreter):
-        assert interpreter.eval("+2") == 2
+        assert run_interpreter(interpreter, "x:=+2") == {'x': 2}
 
     def test_unary_expression(self, interpreter):
-        assert interpreter.eval("-2-2") == -4
+        assert run_interpreter(interpreter, "x:=-2-2") == {'x': -4}
 
-    def test_exprression_priority(self, interpreter):
-        assert interpreter.eval("2+2*2") == 6
+    def test_expression_priority(self, interpreter):
+        assert run_interpreter(interpreter, "x:=2+2*2") == {'x': 6}
 
     def test_unary_expression_paren(self, interpreter):
-        assert interpreter.eval("-(2+2)") == -4
+        assert run_interpreter(interpreter, "x:=-(-(2+2))") == {'x': 4}
 
     def test_binary_unary_expression(self, interpreter):
-        assert interpreter.eval("-(2 + 2 * 4)") == -10
+        assert run_interpreter(interpreter, "x:=-(2 + 2 * 4)") == {'x': -10}
+
+    def test_variables_expression(self, interpreter):
+        assert run_interpreter(interpreter, "x:=-1; y:=x+1; z:=y*x") == {'x': -1, 'y': 0, 'z': 0}
+
+    def test_nested_blocks(self, interpreter):
+        assert run_interpreter(interpreter, "x:=5;\n BEGIN\n y:=x+1;\n END\n") == {'x': 5, 'y': 6}
+
+    def test_unknown_token(self, interpreter):
+        with pytest.raises(SyntaxError):
+            run_interpreter(interpreter, "☺")
+
+    def test_unknown_variable(self, interpreter):
+        with pytest.raises(ValueError):
+            run_interpreter(interpreter, "x: =y;")
+
+    def test_invalid_statement(self, interpreter):
+        with pytest.raises(ValueError):
+            run_interpreter(interpreter, "x := x;")
+
+    def test_invalid_token_order(self, interpreter):
+        with pytest.raises(SyntaxError):
+            run_interpreter(interpreter, "1; 2")
+
+    def test_invalid_node(self, interpreter):
+        with pytest.raises(ValueError):
+            interpreter.visit(Node)
+
+    def test_invalid_unary_operator(self, interpreter):
+        with pytest.raises(ValueError):
+            interpreter.visit_unaryop(UnaryOp(Token(TokenType.OPERATOR, "shit"), Number(Token(TokenType.NUMBER, "1"))))
+
+    def test_invalid_binary_operator(self, interpreter):
+        with pytest.raises(ValueError):
+            interpreter.visit_binop(BinOp(Number(Token(TokenType.NUMBER, "1")), Token(TokenType.OPERATOR, "fuck"),
+                                          Number(Token(TokenType.NUMBER, "2"))))
+
+    def test_empty_colon(self, interpreter):
+        with pytest.raises(SyntaxError):
+            run_interpreter(interpreter, ":")
+
+    def test_empty_eol(self, interpreter):
+        assert run_interpreter(interpreter, "\n") == {}
+
+    def test_end_without_dot(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN\nEND")
+
+    def test_invalid_factor(self, interpreter):
+        with pytest.raises(SyntaxError):
+            parser = Parser()
+            parser._current_token = Token(TokenType.DOT, ".")
+            parser.factor()
+
+    def test_invalid_factor_is_null(self, interpreter):
+        with pytest.raises(SyntaxError):
+            Parser().factor()
